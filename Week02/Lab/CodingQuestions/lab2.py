d6c26682dbe1e86df9accd53e09ac24df6573752
Matthew Macalalad <matthewmacalalad2012@gmail.com>
2025-01-14 10:46:24 -0500

    import random and use the input function

--- Week02/Lab/CodingQuestions/lab2.py
+++ Week02/Lab/CodingQuestions/lab2.py
@@ -1,6 +1,8 @@
 # Coding Questions Week 02
 # Matthew Macalalad, 101510305
 
+import random
+
 elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon"]
 print("Elements: ", elements)
 
@@ -23,3 +25,7 @@ def get_valid_int_input(prompt):
             print("Error: Please enter a valid integer")
             continue
 
+try:
+    elements_selected = get_valid_int_input("Enter the index of the element you like: ")
+except Exception as e:
+    print("")
