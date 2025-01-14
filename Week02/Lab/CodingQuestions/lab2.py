ec12e2538b86381fb645dcd7a8354b1f61cfe972
Matthew Macalalad <matthewmacalalad2012@gmail.com>
2025-01-14 10:54:43 -0500

    used randint to roll for element, and if elif else conditions

--- Week02/Lab/CodingQuestions/lab2.py
+++ Week02/Lab/CodingQuestions/lab2.py
@@ -27,5 +27,15 @@ def get_valid_int_input(prompt):
 
 try:
     elements_selected = get_valid_int_input("Enter the index of the element you like: ")
+    # Roll dice
+    element_roll = random.randint(1, 6)
+    totalNum = elements_selected + element_roll
+    # Print the result based on the totalNum
+    if element_roll <= 2:
+        print("You rolled a weak element, friend.")
+    elif element_roll <= 4:
+        print("Your element is moderately strong.")
+    else:
+        print("Nice element.")
 except Exception as e:
     print("")
