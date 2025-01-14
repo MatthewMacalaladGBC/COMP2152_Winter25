a0c2534a5c78e4a26f111e237680b811c7e8eabe
Matthew Macalalad <matthewmacalalad2012@gmail.com>
2025-01-14 10:17:07 -0500

    add elements array

---
+++ Week02/Lab/CodingQuestions/lab2.py
@@ -0,0 +1,7 @@
+# Coding Questions Week 02
+# Matthew Macalalad, 101510305
+
+elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon"]
+print("Elements: ", elements)
+
+# git add . && git commit -m "add elements array" && git push
