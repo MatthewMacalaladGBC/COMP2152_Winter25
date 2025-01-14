f0883f55552818a21ee48044afdd5b452d171e53
Matthew Macalalad <matthewmacalalad2012@gmail.com>
2025-01-14 10:23:02 -0500

    add pseudo code for python functions

--- Week02/Lab/CodingQuestions/lab2.py
+++ Week02/Lab/CodingQuestions/lab2.py
@@ -5,3 +5,6 @@ elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon"]
 print("Elements: ", elements)
 
 # git add . && git commit -m "add elements array" && git push
+
+# def func_name():
+#        return True
