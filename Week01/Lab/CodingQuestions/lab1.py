09f998ac36234eb4975292c34dda7ed89cbed088
Matthew Macalalad <matthewmacalalad2012@gmail.com>
2025-01-13 21:26:46 -0500

    Week 1 lab coding questions commit

---
+++ Week01/Lab/CodingQuestions/lab1.py
@@ -0,0 +1,18 @@
+# Sample Coding Questions 01 Week 01
+# Matthew Macalalad, 101510305
+
+array = [1, 4, 7, 9]
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = a - ((b ** c) // d) + (a % c)
+# Equals 0 => 1 - ((2 ** 3) // 4) + (1 % 3) => 1 - (8 // 4) + 1 => 1 - 2 + 1 => 0
+
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius.".format(temperature))
+
+userAge = input("Enter your age: ")
+print("Now showing the shop items filtered by age: ", userAge)
