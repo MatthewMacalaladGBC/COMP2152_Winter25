921233d50721a00fd7587d73ca83ec3bc24eaecc
Matthew Macalalad <matthewmacalalad2012@gmail.com>
2025-02-04 10:08:07 -0500

    added assignment 1 file to git

---
+++ Assignments/Assignment1/assignment1_101510305.py
@@ -0,0 +1,9 @@
+"""
+AUTHOR: Matthew Macalalad
+Assignment: #1
+"""
+
+gym_member = "Alex Alliton" # String
+preferred_weight_kg = 20.5 # Float
+highest_reps = 25 # Integer
+membership_active = True # Boolean
