b61a0531acc5acc2fdab8f940d64796e674c01a9
Matthew Macalalad <matthewmacalalad2012@gmail.com>
2025-03-20 21:38:00 -0400

    moved attack functions from functions.py into respective classes

--- Assignments/Assignment2/hero.py
+++ Assignments/Assignment2/hero.py
@@ -7,3 +7,37 @@ class Hero(Character):
 
     def __del__(self):
         super().__del__("Hero")
+
+    # Hero's Attack Function
+    def hero_attacks(self, monster):
+        ascii_image = """
+                                    @@   @@ 
+                                    @    @  
+                                    @   @   
+                @@@@@@          @@  @    
+                @@       @@        @ @@     
+            @%         @     @@@ @       
+                @        @@     @@@@@     
+                @@@@@        @@       
+                @    @@@@                
+            @@@ @@                        
+        @@     @                         
+    @@*       @                          
+    @        @@                          
+            @@                                                    
+            @   @@@@@@@                    
+            @            @                  
+        @              @                  
+
+    """
+        print(ascii_image)
+        print("    |    Player's weapon (" + str(self.combat_strength) + ") ---> Monster (" + str(monster.health_points) + ")")
+        if self.combat_strength >= monster.health_points:
+            # Player was strong enough to kill monster in one blow
+            monster.health_points = 0
+            print("    |    You have killed the monster")
+        else:
+            # Player only damaged the monster
+            monster.health_points -= self.combat_strength
+
+            print("    |    You have reduced the monster's health to: " + str(monster.health_points))
--- Assignments/Assignment2/monster.py
+++ Assignments/Assignment2/monster.py
@@ -7,4 +7,32 @@ class Monster(Character):
 
     def __del__(self):
         super().__del__("Monster")
-        
+    
+    # Monster's Attack Function
+    def monster_attacks(self, hero):
+        ascii_image2 = """                                                                 
+            @@@@ @                           
+        (     @*&@  ,                         
+        @               %                       
+        &#(@(@%@@@@@*   /                      
+        @@@@@.                                
+                @       /                    
+                    %         @                 
+                ,(@(*/           %              
+                @ (  .@#                 @   
+                            @           .@@. @
+                    @         ,              
+                        @       @ .@          
+                                @              
+                            *(*  *      
+                """
+        print(ascii_image2)
+        print("    |    Monster's Claw (" + str(self.combat_strength) + ") ---> Player (" + str(hero.health_points) + ")")
+        if self.combat_strength >= hero.health_points:
+            # Monster was strong enough to kill player in one blow
+            hero.health_points = 0
+            print("    |    Player is dead")
+        else:
+            # Monster only damaged the player
+            hero.health_points -= self.combat_strength
+            print("    |    The monster has reduced Player's health to: " + str(hero.health_points))
